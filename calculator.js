75ea984f9d18cd0f9786a2e5a993e4352062dd62
Siddhu1009 <135247202+Siddhu1009@users.noreply.github.com>
2024-03-18 17:06:56 +0530

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,41 @@
+function combine(value) {
+  var input = document.getElementById('inputb');
+  input.value += value;
+}
+
+function clearInput() {
+  document.getElementById("inputb").value=" ";
+}
+
+function calculate() {
+  var input = document.getElementById('inputb');
+  var result;
+  
+  try {
+    result = eval(input.value);
+    if (!isNaN(result)) {
+      input.value = result;
+    } else {
+      input.value = 'Error';
+    }
+  } catch (error) {
+    input.value = 'Error';
+  }
+}
+
+function backspace() {
+  var input = document.getElementById('inputb');
+  var currentValue = input.value;
+  input.value = currentValue.slice(0, -1);
+}
+
+function root() {
+  var input = document.getElementById('inputb');
+  var value = parseFloat(input.value);
+  
+  if (value >= 0) {
+    input.value = Math.sqrt(value);
+  } else {
+    input.value = 'Error';
+  }
+}
